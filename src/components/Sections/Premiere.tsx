7d16343611841527fb46d4865d10582a192e1352
mradkhh <adkhambekyusupov@gmail.com>
2023-06-26 08:01:45 +0500

    change images in Premiere.tsx

--- src/components/Sections/Premiere.tsx
+++ src/components/Sections/Premiere.tsx
@@ -1,22 +1,22 @@
 import React, {FC, memo} from 'react';
 import Container from "components/Container/Container";
 import Image from "next/image";
-import thor from 'static/images/thor.jpg'
-import jurassic from 'static/images/jurassic.webp'
-import impossible from 'static/images/impossibe.jpg'
+import m1 from 'static/images/m1.jpg'
+import m2 from 'static/images/m2.jpg'
+import m3 from 'static/images/m3.jpg'
 import FilmsCarouselLayout from "layouts/FilmsCarouselLayout";
 import Banner from "components/UI/Banner/Banner";
 import cl from './styles/Premiere.module.scss'
 
 const data = [
     {
-        component: ( <Banner image={thor} /> )
+        component: ( <Banner image={m1} /> )
     },
     {
-        component: ( <Banner image={jurassic} /> )
+        component: ( <Banner image={m2} /> )
     },
     {
-        component: ( <Banner image={impossible} /> )
+        component: ( <Banner image={m3} /> )
     },
 ]
 
